87399eae963b30d66e264940c4da2b5405dc004a
DaniilSmirn0v <d1n.sm1rnv@gmail.com>
2022-03-28 12:38:55 +0300

    Initial Commit

---
+++ Randomizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Randomizer
+//
+//  Created by Даниил Смирнов on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
